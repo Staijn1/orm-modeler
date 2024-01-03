d9cd7882a40f2d76fa950cd790d9eb1afb4502a3
Stein Jonker <stein@jnkr.eu>
2024-01-03 15:04:34 +0100

    Styling for when no state is provided

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,3 @@
 import {Routes} from '@angular/router';
-import {OrmEditorComponent} from "./orm-editor/orm-editor.component";
-import {AppComponent} from "./app.component";
 
-export const routes: Routes = [
-  {path: '', component: AppComponent}
-];
+export const routes: Routes = [];
